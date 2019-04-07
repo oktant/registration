009848a43af7ba6522c957e80fad2d179afe249a
Oktay <oktay.alizada@rectasolutions.com>
2019-04-07 14:59:05 +0200

    Test Service test

---
+++ src/test/java/com/rectasolutions/moving/registration/services/CountryServiceTest.java
@@ -0,0 +1,16 @@
+package com.rectasolutions.moving.registration.services;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CountryServiceTest {
+
+    @Test
+    public void getAlCountries() {
+    }
+
+    @Test
+    public void getCountry() {
+    }
+}
